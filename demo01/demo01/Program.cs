da0e4480b94913b2c9fd0d2f2abb32bd943db975
surya <m.suryaprakash95@gmail.com>
2019-07-21 12:35:46 +0530

    adding demo01

---
+++ demo01/demo01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+
+            
+        }
+
+        public static string createMessage()
+        {
+            return "Hello SURYA";
+        }
+    }
+}
